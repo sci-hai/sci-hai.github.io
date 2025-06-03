6b8cf7af3d43830d5a90ba90c5df6017e81f79fe
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2025-06-03 00:10:09 -0700

    Add openreview link

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -40,7 +40,11 @@ const navbar = (
   <Navbar
     logo={<b>LM4Sci Workshop</b>}
     logoLink="/"   
-    projectLink="https://github.com/sci-hai/lm4sci.github.io"
+    // projectLink="https://github.com/lm4sci/lm4sci.github.io"
+    chatLink="https://openreview.net/group?id=colmweb.org/COLM/2025/Workshop/LM4Sci"
+    chatIcon={
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-input-icon lucide-file-input"><path d="M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M2 15h10"/><path d="m9 18 3-3-3-3"/></svg>
+    }
   />
 );
 const footer = (
